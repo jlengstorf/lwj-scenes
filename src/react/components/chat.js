4e049f6e9bbdbd34059f142ba5f9dc4eff7d3468
Jason Lengstorf <jason@lengstorf.com>
2020-05-17 19:14:54 -0700

    fix: remove test messages

--- src/react/components/chat.js
+++ src/react/components/chat.js
@@ -3,49 +3,11 @@ import { useMachine } from '@xstate/react';
 import { Machine, assign } from 'xstate';
 import { useChat } from '../hooks/use-chat';
 
-const MESSAGES = new Map();
-
-MESSAGES.set('0', {
-  author: {
-    username: 'jlengstorf',
-    roles: ['BROADCASTER'],
-  },
-  html: 'this is a broadcaster message',
-  time: 1233,
-});
-
-MESSAGES.set('1', {
-  author: {
-    username: 'someonecool',
-    roles: ['MODERATOR'],
-  },
-  html: 'this is a moderator message',
-  time: 1234,
-});
-
-MESSAGES.set('2', {
-  author: {
-    username: 'subfriend',
-    roles: ['SUBSCRIBER'],
-  },
-  html: 'this is a subscriber message',
-  time: 1235,
-});
-
-MESSAGES.set('3', {
-  author: {
-    username: 'newfriend',
-    roles: [],
-  },
-  html: 'this is a general message',
-  time: 1236,
-});
-
 const chatMachine = Machine({
   id: 'chat',
   initial: 'idle',
   context: {
-    messages: MESSAGES, //new Map(),
+    messages: new Map(),
   },
   states: {
     idle: {
@@ -82,13 +44,9 @@ const Chat = () => {
   const [state, send] = useMachine(chatMachine);
   const message = useChat();
 
-  console.log({ message });
-
   useEffect(() => {
     send('CHAT_RECEIVED', { message });
   }, [message, send]);
-
-  console.log({ context: state.context });
 
   return (
     <ul style={{ color: 'var(--text-muted)' }}>
